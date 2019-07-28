6709a7b6cfd470413a7199505360170cc1796b0a
Andres Carranza <diacarry@gmail.com>
2019-07-28 12:46:33 -0500

    Archivos iniciales del proyecto en angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { Routes, RouterModule } from '@angular/router';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { IndexPageComponent } from './Pages/index-page/index-page.component';
+
+const rutas: Routes = [
+  {path: '', component: IndexPageComponent},
+  {path: 'index', component: IndexPageComponent}/*,
+  {path: 'register', component: RegisterPageComponent},
+  {path: 'people', component: RegisterListPageComponent},
+  {path: 'partner', component: PartnerListPageComponent}*/
+];
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(rutas)
   ],
   providers: [],
   bootstrap: [AppComponent]
